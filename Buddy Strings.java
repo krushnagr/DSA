b0967a17a3f0840fbb51a507b93a1854a8c1a851
krushnagr <123489852+krushnagr@users.noreply.github.com>
2023-07-03 19:45:20 +0530

    Create Buddy Strings.java

---
+++ Buddy Strings.java
@@ -0,0 +1,46 @@
+/*
+Buddy Strings
+Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+
+For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+Input: s = "ab", goal = "ba"
+Output: true
+Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+*/
+
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        int n = s.length();
+        if (s.equals(goal)) {
+            Set<Character> temp = new HashSet<>();
+            for (char c : s.toCharArray()) {
+                temp.add(c);
+            }
+            return temp.size() < goal.length(); // Swapping same characters
+        }
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j && s.charAt(i) == goal.charAt(i)) {
+            i++;
+        }
+
+        while (j >= 0 && s.charAt(j) == goal.charAt(j)) {
+            j--;
+        }
+
+        if (i < j) {
+            char[] sArr = s.toCharArray();
+            char temp = sArr[i];
+            sArr[i] = sArr[j];
+            sArr[j] = temp;
+            s = new String(sArr);
+        }
+
+        return s.equals(goal);
+
+    }
+}
